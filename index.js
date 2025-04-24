a6031bffba774d4cc9f39c5e9c743a4d562cb434
Dorlig <asger@kragmorild.dk>
2025-04-24 09:10:14 +0200

    added files

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
-
 const app = express();
-const PORT = 3000;
 
-app.listen(PORT, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
-    else 
-        console.log("Error occurred, server can't start", error);
-    }
-);
+// Use express.static middleware to serve static files from the "public" folder
+app.use(express.static('public'));
+
+// Define a route (this is optional)
+app.get('/', (req, res) => {
+  res.send('Welcome to my static website!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
