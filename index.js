dfcae90bbe21044a1d54bd0fd5c2b44ca5ae5fdd
Dorlig <asger@kragmorild.dk>
2025-04-24 12:50:29 +0200

    added sign in and sign up functions

--- index.js
+++ index.js
@@ -1,29 +1,127 @@
 const e = require('express');
 const express = require('express');
+const bodyParser = require('body-parser');
 var fs = require('fs');
 const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + "/views"));
 
+const tempUser = {
+            "password": "",
+            "email": "",
+            "friends": [],
+            "profile": {
+                "age": -1,
+                "interests": [],
+                "description": ""
+            },
+            "chats": {},
+            "activities": []
+            
+        }
+
+const tempMessage ={"origin": "",
+    "content": "",
+    "time": ""}
+
+const tempActivity = {
+    "title": "",
+    "created": "",
+    "description": "",
+    "friends": [],
+    "participants": []
+}
+
 app.get('/', (req, res) => {
     res.render('home');
 });
 app.get('/login', (req, res) => {
+    console.log(req.body)
+
     res.render('login');
-    console.log(req).body
+});
+app.post('/login', (req, res) => {
+    const {username, password} = req.body
+
+    var obj = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+
+    console.log(username, password)
+    console.log(obj.users[username])
+
+    if (obj.users[username] && obj.users[username].password === password) {
+        res.redirect("/friends/"+username)
+    }
+    else {
+        res.render('login');
+    }
 });
 
-app.get('/login-request', (req, res) => {
-    // console.log(req)
+app.get('/signUp', (req, res) => {
+    // console.log(req.body)
 
-    res.render('login');
+    res.render('signUp');
+});
+app.post('/signUp', (req, res) => {
+    const {username, email, password} = req.body
+
+    var obj = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+
+    // console.log(obj.users)
+
+    const emailUsed = false
+    for (const user of Object.keys(obj.users)) {
+        if (email == obj.users[user].email) {
+            emailUsed = true
+        }
+    }
+    
+    if (!obj.users[username] && !emailUsed){
+        // Create profile
+        const newUser = structuredClone(tempUser)
+        newUser.username = username
+        newUser.password = password
+        newUser.email = email
+
+        obj.users[username] = newUser
+
+        const data = JSON.stringify(obj);
+
+        fs.writeFile("data.json", data, (error) => {
+            if (error) {
+                console.error(error);
+
+                throw error;
+            }
+
+            console.log("created new User");
+        });
+
+        res.redirect("/profile/"+username)
+    }
+    else {
+        res.render('signUp');
+    }
+    
+
+    // var obj = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+
+    // console.log(username, password)
+    // console.log(obj.users[username])
+
+    // if (obj.users[username] && obj.users[username].password === password) {
+    //     res.redirect("/friends/"+username)
+    // }
+    // else {
+    //     res.render('signUp');
+    // }
 });
 
 app.get('/settings', (req, res) => {
@@ -81,9 +179,19 @@ app.get('/searchActivity', (req, res) => {
 app.get('/createActivity', (req, res) => {
     res.render('createActivity');
 });
-app.get('/profile', (req, res) => {
+app.get('/profile/:username', (req, res) => {
+    var obj = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+
+
+
+    res.render('profile', {profile: obj.users[username].profile});
+});
+app.post('/profile/:username', (req, res) => {
+    var obj = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+
     res.render('profile');
 });
+
 app.get('/activity', (req, res) => {
     res.render('activity');
 });
