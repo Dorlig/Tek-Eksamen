e85ed03c1438552d823e08ab1a9097943f5846f7
Dorlig <asger@kragmorild.dk>
2025-04-24 09:30:19 +0200

    added html files

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+const PORT = 3000;
 
-// Use express.static middleware to serve static files from the "public" folder
-app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
-// Define a route (this is optional)
 app.get('/', (req, res) => {
-  res.send('Welcome to my static website!');
+    res.render('home');
+});
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+app.get('/settings', (req, res) => {
+    res.render('settings');
+});
+app.get('/friends', (req, res) => {
+    res.render('friends');
+});
+app.get('/activities', (req, res) => {
+    res.render('activities');
+});
+app.get('/chat', (req, res) => {
+    res.render('chat');
+});
+app.get('/call', (req, res) => {
+    res.render('call');
+});
+app.get('/searchFriends', (req, res) => {
+    res.render('searchFriends');
+});
+app.get('/searchActivity', (req, res) => {
+    res.render('searchActivity');
+});
+app.get('/createActivity', (req, res) => {
+    res.render('createActivity');
+});
+app.get('/profile', (req, res) => {
+    res.render('profile');
+});
+app.get('/activity', (req, res) => {
+    res.render('activity');
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
